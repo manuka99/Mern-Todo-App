c5cc9cefdac4a0e37e25dbc71aacc069e4879153
Manuka Yasas <manukayasas99@gmail.com>
2024-01-31 21:52:41 +0530

    feat: ui updated

---
+++ frontend/src/app/components/Todo/TodoItem/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Todo } from "@/app/model/Todo.m";
+import React from "react";
+
+interface TodoItem {
+  data: Todo;
+  deleteTodo: (id: string) => Promise<void>;
+}
+
+const TodoItem = ({ data, deleteTodo }: TodoItem) => {
+  return (
+    <div className="flex w-full h-[60px] items-center justify-between bg-gray-800 p-4 rounded-md">
+      <p className="capitalize">{data.description}</p>
+      <button
+        className="bg-red-800 text-sm font-bold py-1 px-2 rounded-md"
+        onClick={() => deleteTodo(data._id)}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TodoItem;
