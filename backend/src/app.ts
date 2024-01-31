e912981a36d0a5dd495c88d2481d714b6ab6a46e
Manuka Yasas <manukayasas99@gmail.com>
2024-01-31 19:28:28 +0530

    feat: error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import todoRoutes from "./routes";
+import { handleError } from "./middlewares/handleError";
 
 const app: Express = express();
 
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // routes
-app.use(todoRoutes);
+app.use("/api", todoRoutes);
+
+/* ERRORS */
+app.use(handleError);
 
 export default app;
--- backend/src/controllers/todos/index.ts
+++ backend/src/controllers/todos/index.ts
@@ -1,17 +1,26 @@
-import { Response, Request } from "express";
+import { Response, Request, NextFunction } from "express";
 import { ITodo } from "../../types/todo";
 import Todo from "../../models/todo";
+import { sendSuccess } from "../../common/util";
 
-const getTodos = async (req: Request, res: Response): Promise<void> => {
+const getTodos = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const todos: ITodo[] = await Todo.find();
-    res.status(200).json({ todos });
+    sendSuccess(res, todos);
   } catch (error) {
-    throw error;
+    next(error);
   }
 };
 
-const addTodo = async (req: Request, res: Response): Promise<void> => {
+const addTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const body = req.body as Pick<ITodo, "title" | "description">;
     const todo: ITodo = new Todo({
@@ -19,52 +28,59 @@ const addTodo = async (req: Request, res: Response): Promise<void> => {
       description: body.description,
       isCompleted: false,
     });
-    const newTodo: ITodo = await todo.save();
-    const allTodos: ITodo[] = await Todo.find();
-    res
-      .status(201)
-      .json({ message: "Todo added", todo: newTodo, todos: allTodos });
+    const newItem: ITodo = await todo.save();
+    sendSuccess(res, {
+      message: "Todo added",
+      newItem,
+    });
   } catch (error) {
-    throw error;
+    next(error);
   }
 };
 
-const updateTodoStatus = async (req: Request, res: Response): Promise<void> => {
+const updateTodoStatus = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id = req.params.id;
     const { isCompleted } = req.body as Pick<ITodo, "isCompleted">;
 
-    const updatedTodo: ITodo | null = await Todo.findByIdAndUpdate(
+    const updatedItem: ITodo | null = await Todo.findByIdAndUpdate(
       { _id: id },
       {
         isCompleted,
+      },
+      {
+        new: true,
       }
     );
 
-    const allTodos: ITodo[] = await Todo.find();
-    res.status(200).json({
+    sendSuccess(res, {
       message: "Todo updated",
-      todo: updatedTodo,
-      todos: allTodos,
+      updatedItem,
     });
   } catch (error) {
-    throw error;
+    next(error);
   }
 };
 
-const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+const deleteTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
-    const deletedTodo: ITodo | null = await Todo.findByIdAndDelete(
+    const deletedItem: ITodo | null = await Todo.findByIdAndDelete(
       req.params.id
     );
-    const allTodos: ITodo[] = await Todo.find();
-    res.status(200).json({
+    sendSuccess(res, {
       message: "Todo deleted",
-      todo: deletedTodo,
-      todos: allTodos,
+      deletedItem,
     });
-  } catch (err) {
-    throw err;
+  } catch (error) {
+    next(error);
   }
 };
 
